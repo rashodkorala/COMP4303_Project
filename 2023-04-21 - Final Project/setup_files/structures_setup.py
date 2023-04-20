e42faec8e0e373255caf36f8aff4959cbc05ec38
VilakshanKh <31506996+Vilakshankh@users.noreply.github.com>
2023-04-20 07:14:29 -0230

    added building placement methods

---
+++ 2023-04-21 - Final Project/setup_files/structures_setup.py
@@ -0,0 +1,85 @@
+from structures.nbt.convert_nbt import convert_nbt
+from structures.nbt.nbt_asset import NBTAsset
+from structures.structure import Structure
+from structures.transformation import Transformation
+from gdpc.editor import Editor
+from gdpc.block import Block
+from palette.palette import Palette
+from palette.palette_swap import palette_swap
+import os
+import random
+
+# returns the dimensions of the structure
+def get_building_dimensions(filepath):
+    structure = convert_nbt(filepath)
+    return structure.width, structure.height, structure.depth 
+
+
+#creates the structure from the nbt file
+def create_nbt_structure(filepath, editor, buildArea, x, z, skips_air=True):
+    structure = convert_nbt(filepath)
+
+    #create nbt structure
+    for (pos, palette_index) in structure.blocks.items():
+        block = structure.palette[palette_index]
+        if block.to_gdpc_block().id == "minecraft:air" and skips_air:
+            continue
+        build_pos = buildArea.begin + pos+[x,0,z]
+        editor.placeBlock(position=build_pos, block=block.to_gdpc_block())
+        #print(build_pos, block.to_gdpc_block())
+
+
+def get_files(folder_path):
+    file_paths = []
+
+    for foldername, subfolders, filenames in os.walk(folder_path):
+        for filename in filenames:
+            if filename.endswith('.nbt'):
+                full_path = os.path.join(foldername, filename)
+                file_paths.append(full_path)
+                print("Current folder path plus name of file: ", full_path)
+
+    return file_paths
+
+
+
+# Check if a building overlaps with any other existing buildings
+def is_overlapping(new_building, existing_buildings):
+    for building in existing_buildings:
+        if not (new_building["x"] + new_building["width"] <= building["x"] or
+                new_building["x"] >= building["x"] + building["width"] or
+                new_building["z"] + new_building["depth"] <= building["z"] or
+                new_building["z"] >= building["z"] + building["depth"]):
+            return True
+    return False
+
+
+# Generate random building position within the build area
+def random_building_position(building_width, building_depth, build_area_size):
+    x = random.randint(0, build_area_size.x - building_width)
+    z = random.randint(0, build_area_size.z - building_depth)
+    return x, z
+
+
+def place_buildings(file_paths, build_area_size, editor, max_attempts=100):
+    existing_buildings = []
+
+    for building in file_paths:
+        building_width, building_height, building_depth = get_building_dimensions(building)
+
+        # Try to find a non-overlapping position for the building
+        attempts = 0
+        while attempts < max_attempts:
+            x, z = random_building_position(building_width, building_depth, build_area_size)
+            new_building = {"x": x, "z": z, "width": building_width, "depth": building_depth}
+
+            if not is_overlapping(new_building, existing_buildings):
+                existing_buildings.append(new_building)
+                create_nbt_structure(building, editor, build_area, x, z, skips_air=True)
+                break
+            attempts += 1
+
+        if attempts == max_attempts:
+            print(f"Failed to place {building} without overlapping after {max_attempts} attempts.")
+
+    return existing_buildings
