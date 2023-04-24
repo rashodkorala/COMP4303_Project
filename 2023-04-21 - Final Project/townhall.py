a5ab65289f92b53c7fc827bd5b318140f46e7e23
rashodk <rmanamperiko@mun.ca>
2023-04-24 15:46:16 -0230

    townhall donee

--- 2023-04-21 - Final Project/townhall.py
+++ 2023-04-21 - Final Project/townhall.py
@@ -117,11 +117,11 @@ def make_townhall(editor, starting_pos, wall_block_type, roof_block_type, floor_
     # Define dimensions and parameters
     length = 20
     width = 10
-    wall_height = 10
+    wall_height = 5
     roof_height = 5
     line_height = 1
     size_reduction = 1
-    """
+    
     # Create walls
     for x in range(length):
         for y in range(2 * wall_height):
@@ -151,10 +151,11 @@ def make_townhall(editor, starting_pos, wall_block_type, roof_block_type, floor_
                         for j in range(window_height):
                             position = starting_pos + np.array([x + i-1, y + j, 0])
                             editor.placeBlock(position, Block(window_block_type))
+                            editor.placeBlock(position+np.array([0,0,width-1]), Block(window_block_type))
 
     # Create roof
-    roof_starting_pos = starting_pos + np.array([length // 2 - 1, 2 * wall_height - 1, width // 2 - 1])
-    make_roof(editor, length + 3, width + 4, roof_height + 4, roof_starting_pos, roof_block_type, line_height, size_reduction)
+    roof_starting_pos = starting_pos + np.array([length // 2 - 1, 2 * wall_height   , width // 2 - 1])
+    make_roof(editor, length, width, roof_height, roof_starting_pos, roof_block_type, line_height, size_reduction)
 
     # Create entrance
     door_height = 3
@@ -162,26 +163,74 @@ def make_townhall(editor, starting_pos, wall_block_type, roof_block_type, floor_
     door_start_x = (length - door_width) // 2
     door_start_z = 0
     for x in range(door_start_x, door_start_x + door_width):
-        for y in range(door_height):
-            position = starting_pos + np.array([x, y+1, door_start_z])
-            editor.placeBlock(position, Block('air'))
-
-
- """
+        position = starting_pos + np.array([x, 1, door_start_z])
+        # change the hinge to the other side
+        if x == door_start_x:
+            editor.placeBlock(position, Block('spruce_door[facing=north,half=lower,hinge=left,open=false,powered=false]'))
+        else:
+            editor.placeBlock(position, Block('spruce_door[facing=north,half=lower,hinge=right,open=false,powered=false]'))
+        editor.placeBlock(position + np.array([0, 2,0 ]), Block('stone_brick_stairs[facing=south,half=top,shape=straight,waterlogged=false]'))
+        
     # Create staircase
-    staircase_height = wall_height
-    staircase_width = wall_height
-    staircase_start_x = (length - staircase_width) // 2
-    staircase_start_z = (width - staircase_width) // 2
-    for x in range(staircase_start_x, staircase_start_x + staircase_width):
-        for y in range(staircase_height):
-            for z in range(staircase_start_z, staircase_start_z + staircase_width):
-                if x == staircase_start_x or x == staircase_start_x + staircase_width - 1 or z == staircase_start_z or z == staircase_start_z + staircase_width - 1:
-                    position = starting_pos + np.array([x, y, z])
-                    editor.placeBlock(position, Block(staircase_block_type))
-                else:
-                    position = starting_pos + np.array([x, y, z])
-                    editor.placeBlock(position, Block("minecraft:air"))
+    for i in range(1,wall_height):
+        pos=starting_pos + np.array([0, i, i])
+        editor.placeBlock(pos , Block("minecraft:glass_pane"))
+        pos=starting_pos + np.array([length//2+i-2, i, length //2 -2])
+        editor.placeBlock(pos , Block("minecraft:spruce_stairs[facing=east,half=bottom,shape=straight,waterlogged=false]"))
+        editor.placeBlock(pos+np.array([0,0,-1]) , Block("minecraft:spruce_stairs[facing=east,half=bottom,shape=straight,waterlogged=false]"))
+        
+        editor.placeBlock(starting_pos+np.array([length//2+i-2,wall_height,length//2-2]) , Block("air")) 
+        editor.placeBlock(starting_pos+np.array([length//2+i-2,wall_height,length//2-2-1]) , Block("air"))     
+
+    
+    #create a table right left of the entrance
+    for z in range(2,width-2):
+        for x in range(3,5):
+            editor.placeBlock(starting_pos+np.array([x,1,z]) , Block("spruce_slab[type=top,waterlogged=false]"))
+            editor.placeBlock(starting_pos+np.array([x,2,z]) , Block("red_carpet"))
+    
+    #place lanterns above the table
+    for z in range(2,width-2):
+        for x in range(3,5):
+            editor.placeBlock(starting_pos+np.array([x,wall_height-1,z]) , Block("lantern[hanging=true]"))
+
+    # place lanterns right corners of the entrance
+    editor.placeBlock(starting_pos+np.array([length-2,wall_height-1,width-2]) , Block("lantern[hanging=true]"))
+    editor.placeBlock(starting_pos+np.array([length-2,wall_height-1,1]) , Block("lantern[hanging=true]"))
+
+    editor.placeBlock(starting_pos+np.array([length-2,2*wall_height-1,width-2]) , Block("lantern[hanging=true]"))
+    editor.placeBlock(starting_pos+np.array([length-2,2*wall_height-1,1]) , Block("lantern[hanging=true]"))
+
+    # editor.placeBlock(starting_pos+np.array([length-2,2*wall_height-1,width-2]) , Block("lantern[hanging=true]"))
+    # editor.placeBlock(starting_pos+np.array([length-2,2*wall_height-1,1]) , Block("lantern[hanging=true]"))
+
+    editor.placeBlock(starting_pos+np.array([1,2*wall_height-1,1]) , Block("lantern[hanging=true]"))
+    editor.placeBlock(starting_pos+np.array([1,2*wall_height-1,width-2]) , Block("lantern[hanging=true]"))
+
+    
+
+
+    #palce a bookshelf
+    for z in range(3,width-3):
+        for i in range(1,3):
+            editor.placeBlock(starting_pos+np.array([length-2,2*wall_height-i,z]) , Block("bookshelf"))
+    
+    #place a table under the bookshelf
+    for z in range(3,width-3):
+        editor.placeBlock(starting_pos+np.array([length-2,2*wall_height-4,z]) , Block("spruce_slab[type=top,waterlogged=false]"))
+    
+    #place two blocks next to table on both sides
+    editor.placeBlock(starting_pos+np.array([length-2,2*wall_height-4,2]) , Block("oak_planks"))
+    editor.placeBlock(starting_pos+np.array([length-2,2*wall_height-4,width-3]) , Block("oak_planks"))
+
+    #place a lantern on top
+    editor.placeBlock(starting_pos+np.array([length-2,2*wall_height-3,2]) , Block("lantern[hanging=false]"))
+    editor.placeBlock(starting_pos+np.array([length-2,2*wall_height-3,width-3]) , Block("lantern[hanging=false]"))
+
+    #place some leaves 
+    editor.placeBlock(starting_pos+np.array([length-2,2*wall_height-1,2]) , Block("spruce_leaves"))
+    editor.placeBlock(starting_pos+np.array([length-2,2*wall_height-1,width-3]) , Block("spruce_leaves"))
+
 
 
 # Call the function to create the townhall
@@ -191,7 +240,7 @@ roof_block_type = 'stone'
 floor_block_type = 'oak_planks'
 window_block_type = 'glass_pane'
 staircase_block_type = 'oak_stairs'
-# make_townha ll(editor, starting_pos, wall_block_type, roof_block_type, floor_block_type, window_block_type, staircase_block_type)
+make_townhall(editor, starting_pos, wall_block_type, roof_block_type, floor_block_type, window_block_type, staircase_block_type)
 
 
 def create_pyramid_and_reflection_in_xz_plane(editor, starting_pos, block_type, height, building_h=20):
@@ -228,12 +277,12 @@ def create_pyramid_and_reflection_in_xz_plane(editor, starting_pos, block_type, 
     
 
 # Set the height of the pyramid
-height = 8
+height = 5
 starting_pos =buildArea.begin+np.array([0,0,0])
 block_type = 'stone_bricks'
 
 # Call the function to create the pyramid and its reflection in Minecraft
-create_pyramid_and_reflection_in_xz_plane(editor, starting_pos, block_type, height)
+# create_pyramid_and_reflection_in_xz_plane(editor, starting_pos, block_type, height)
 
 
 
